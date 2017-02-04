eb5dde26cb7ecca41fb0ae5a8be7125c94b960f6
justUmen <u-men@outook.com>
2017-02-05 00:08:27 +0700

    add details about the script

--- better_history.sh
+++ better_history.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 #Creator : https://github.com/justUmen
-# This program enhances the access of the user's history with regex and a dialog list.
-# The user can then paste the wanted command using Ctrl + Shift + v
-#It is supposed to be use with an alias : alias h="/path/to/this/script"
-#It is supposed to be launched with a simple "h whatever"
-
-#Todo :
-# - support for more shells
-# - usage of ~/.my_history_selection to sort by most used command, rather than last used commands
-# - improve unreliable regex (sed 's/.*:0;//')
-# - version with rofi ?
+#Link : https://github.com/justUmen/better_history
 
 #Check dependancies :
 command -v dialog >/dev/null 2>&1 || {
@@ -18,14 +9,6 @@ command -v dialog >/dev/null 2>&1 || {
 command -v xclip >/dev/null 2>&1 || {
 	echo -e >&2 "Package \"xclip\" is needed\n - Archlinux : sudo pacman -S xclip\n - Debian : sudo apt-get install xclip";exit 231;
 }
-
-# - Use the default shell, based on $SHELL environment variable
-
-# 1 - remove duplicates from the list : awk '!x[$0]++'
-# 2 - remove "h" command itself : sed '/^h /d'
-# 3 - reverse the list, last commands stay top : tac
-# 4 - clean zsh_history : sed 's/.*:0;//'
-# 5 - use readlink if /bin/sh is a link towards /usr/bin/zsh
 
 ARGS=$@
 if [ "$SHELL" == "/usr/bin/zsh" ] || [ "`readlink $SHELL`" == "zsh" ]; then
