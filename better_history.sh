cceb462321850bd79b7e168b360d478e18918926
justUmen <u-men@outook.com>
2017-02-05 15:14:53 +0700

    add rofi

--- better_history.sh
+++ better_history.sh
@@ -14,15 +14,15 @@ command -v xclip >/dev/null 2>&1 || {
 ARGS=$@
 if [ "$SHELL" == "/usr/bin/zsh" ] || [ "`readlink $SHELL`" == "zsh" ]; then
 	#ZSH
-	if [ "$1" == "" ]; then
-		cat ~/.zsh_history | sed 's/.*:0;//' | awk '!x[$0]++' | sed '/^h /d' > ~/.my_history
+	if [ "$1" == "" ] || [ "$1" == "rofi" ]; then
+		cat ~/.zsh_history | sed 's/.*:0;//' | awk '!x[$0]++' | tac | sed '/^h /d' > ~/.my_history
 	else
 		cat ~/.zsh_history | sed 's/.*:0;//' | grep "$ARGS" | tac | awk '!x[$0]++' | sed '/^h /d' > ~/.my_history
 	fi
 else
 	#USE BASH AS DEFAULT (NOT TESTED)
-	if [ "$1" == "" ]; then
-		 cat .bash_history | awk '!x[$0]++' | sed '/^h /d' > ~/.my_history;
+	if [ "$1" == "" ] || [ "$1" == "rofi" ]; then
+		 cat .bash_history | awk '!x[$0]++' | tac | sed '/^h /d' > ~/.my_history;
 	else
 		 cat .bash_history | grep "$ARGS" | tac | awk '!x[$0]++' | sed '/^h /d' > ~/.my_history;
 	fi
@@ -34,19 +34,30 @@ if [ "`wc -l ~/.my_history|sed 's/ .*//'`" == "0" ];then
 	exit 229;
 fi
 
-#Main code
-cmd=(dialog --nocancel --keep-tite --menu "Select command for your clipboard:" 100 100 100)
-options=()
-commands=()
-CMP=1
-while read line; do
-	options+=($CMP "$line")
-	commands+=("$line")
-	CMP=`expr $CMP + 1`
-done < ~/.my_history
-choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-echo "CLIPBOARD : ${commands[`expr $choice - 1`]}"
-echo -n "${commands[`expr $choice - 1`]}" | xclip -selection c
+#Check if argument is rofi, otherwise use dialog
+if [ "$1" == "rofi" ];then
+	command -v dialog >/dev/null 2>&1 || {
+		echo -e >&2 "Package \"rofi\" is needed\n - Archlinux : sudo pacman -S rofi\n - Debian : sudo apt-get install rofi";exit 231;
+	}
+	command=`cat .my_history| rofi -dmenu`
+	echo "$command"  >> ~/.my_history_selection
+else
+	cmd=(dialog --nocancel --keep-tite --menu "Select command for your clipboard:" 100 100 100)
+	options=()
+	commands=()
+	CMP=1
+	while read line; do
+		options+=($CMP "$line")
+		commands+=("$line")
+		CMP=`expr $CMP + 1`
+	done < ~/.my_history
+	choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+	command="${commands[`expr $choice - 1`]}"
+fi
 
-### future usage (see Todo in README.md) ###
-echo "${commands[`expr $choice - 1`]}" >> ~/.my_history_selection
+if [ "$command" != "" ];then
+	echo "CLIPBOARD : $command"
+	echo -n "$command" | xclip -selection c
+	### future usage (see Todo in README.md) ###
+	echo "$command" >> ~/.my_history_selection
+fi
